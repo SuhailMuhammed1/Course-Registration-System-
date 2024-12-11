a83ff5c45aae206017cbc0d28165b07cc3202ee9
SuhailMuhammed1 <suhailjr111@gmail.com>
2024-12-11 22:33:49 +0530

    updated debug and host

--- course_registration_system/course_registration_system/settings.py
+++ course_registration_system/course_registration_system/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'c#ugyk6kkey7l)%$g1a@^ni!!hu(h%@om1s9cg-&cx_psji8b#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
